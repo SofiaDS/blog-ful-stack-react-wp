5fd4172d1cc5514253796034724fbd1ab95bd0ff
poppi <s.dellaspora@outlook.com>
2021-10-04 22:43:13 +0200

    added fontawesome

--- project/src/components/Footer/footer.js
+++ project/src/components/Footer/footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default class Footer extends React.Component {
     render() {
         return (
-        <div className="bg-warning container-fluid">
-            Bye bye
+        <div className="container-fluid">
+            
         </div>
         ) 
         

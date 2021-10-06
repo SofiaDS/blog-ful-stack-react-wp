62a7dcd95b00b86aa73e30431ef0f0d114222cb3
poppi <s.dellaspora@outlook.com>
2021-10-06 22:10:32 +0200

    finished all only proper style missing

--- project/src/components/Header/header.js
+++ project/src/components/Header/header.js
@@ -5,6 +5,8 @@ import Home from "../Home/home.js";
 import Error from "../Error/error.js";
 import DynSection from "../Section/section.js";
 import ArtDetail from "../ArticleDetail/articleDetail.js";
+import { objectToArt } from "../../models/articleModel.js";
+import Pages from "../Pag/pages.js";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class Header extends React.Component {
@@ -13,15 +15,31 @@ class Header extends React.Component {
 
     this.state = {
       cat: [],
+      pages: [],
     };
   }
 
-  componentDidMount() {
+  readCat() {
     fetch("http://epicode.test/bedrock/web/wp-json/wp/v2/categories/")
       .then((res) => res.json())
       .then((categories) => {
         this.setState({ cat: categories });
       });
+    console.log(this.state.cat);
+  }
+
+  readPages() {
+    fetch("http://epicode.test/bedrock/web/wp-json/wp/v2/pages")
+      .then((pages) => pages.json())
+      .then((pages) => {
+        this.setState({ pages: pages.map((page) => objectToArt(page))
+        });
+      });
+  }
+
+  componentDidMount() {
+    this.readCat();
+    this.readPages();
   }
 
   render() {
@@ -30,11 +48,17 @@ class Header extends React.Component {
         <Link to={`/categories/${cat.id}/posts`}>{cat.name}</Link>
       </span>
     ));
+    const pages = this.state.pages.map((page) => (
+      <span key={page.id}>
+        <Link to={`/pages/${page.id}`}>{page.title}</Link>
+      </span>
+    ));
     const navBar = (
       <div className="container-fluid">
         <nav className="navbar navbar expand-expand-lg">
           <img src="images/logo.png" className="logoHome"></img>
           <Link to="/">Home</Link>
+          {pages}
           {category}
         </nav>
         <Switch>
@@ -46,6 +70,9 @@ class Header extends React.Component {
           </Route>
           <Route path="/posts/:id">
             <ArtDetail />
+          </Route>
+          <Route path="/pages/:id">
+            <Pages />
           </Route>
           <Route path="*">
             <Error />
---
+++ project/src/components/Pag/pages.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { withRouter } from "react-router";
+import { Link } from "react-router-dom";
+import { ArticleModel, objectToArt } from "../../models/articleModel.js";
+
+let isLoading = false;
+
+class Pages extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pageContent: [],
+    };
+  }
+
+  read() {
+    isLoading = true;
+
+    fetch(
+      `http://epicode.test/bedrock/web/wp-json/wp/v2/pages/${this.props.match.params.id}`
+    )
+      .then((res) => res.json())
+      .then((page) => {
+        this.setState({
+          pageContent: page,
+        });
+        console.log(this.state.pageContent);
+      });
+  }
+
+  componentDidMount() {
+    this.read();
+  }
+
+  componentDidUpdate() {
+    if (isLoading) {
+      isLoading = false;
+      return;
+    }
+
+    this.read();
+  }
+
+  render() {
+    const cards = (
+      <div>
+        <h1>{this.state.pageContent?.title?.rendered}</h1>
+        <p dangerouslySetInnerHTML={{__html:this.state.pageContent?.content?.rendered}}></p>
+      </div>
+    );
+    return <div> {cards}</div>;
+  }
+}
+export default withRouter(Pages);
